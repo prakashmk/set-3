0991342f2d49ac3cd7595cfcfd814f3add47fa4e
prakashmk <30828492+prakashmk@users.noreply.github.com>
2018-01-23 16:42:31 +0530

    Create p28.c

---
+++ p28.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],i,n;
+printf("enter a limit ");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+scanf("%d",&i);
+}
+for(i=0;i<=n;i++)
+{
+printf("the array elements %d %d",a[i],i);
+}
+}
